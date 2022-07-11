7c32da22c2cbef810a5f433b3752a5159f1e19d0
Wonwolf1834 <90250933+thang1834@users.noreply.github.com>
2022-07-11 14:39:36 +0700

    <3

---
+++ temp/GenMap/GenMap/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GenMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1280, 720);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1298, 572);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 120);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1377, 768);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ temp/GenMap/GenMap/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenMap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Button> ButtonList = new List<Button>();
+        List<int> ClickCount = new List<int>();
+        List<int> Map = new List<int>();
+        List<List<int>> MapRow = new List<List<int>>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int width = 64;
+            int heigth = 36;
+            int sz = 23;
+            flowLayoutPanel1.Size = new Size((sz) * width + 5, (sz) * heigth);
+            for (int i = 0; i < width * heigth; i++)
+            {
+                Button btn = new Button();
+                btn.Size = new Size(sz, sz);
+                btn.Margin = new Padding(0);
+                btn.BackColor = Color.White;
+                //btn.Text = i.ToString();
+                btn.Click += (sender1, e1) =>
+                {
+                    ChangeColor(btn);
+                };
+                flowLayoutPanel1.Controls.Add(btn);
+                ButtonList.Add(btn);
+                ClickCount.Add(0);
+            }
+        }
+
+        void ChangeColor(Button btn)
+        {
+            int index = ButtonList.IndexOf(btn);
+            int Count = ClickCount[index];
+            switch (Count % 5)
+            {
+                // duong di
+                case 0:
+                    btn.Text = "1";
+                    btn.BackColor = Color.Green;
+                    break;
+                // start
+                case 1:
+                    btn.Text = "-1";
+                    btn.BackColor = Color.Yellow;
+                    break;
+                // stop
+                case 2:
+                    btn.Text = "2";
+                    btn.BackColor = Color.Red;
+                    break;
+                // reset
+                case 3:
+                    btn.Text = "3";
+                    btn.BackColor = Color.Gray;
+                    break;
+                case 4:
+                    btn.Text = "";
+                    btn.BackColor = Color.White;
+                    break;
+            }
+            ClickCount[index]++;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = @"int[,] array = new int[,]
+{";
+            int count = 0;
+            for (int i = 0; i < ButtonList.Count; i++)
+            {
+                Button btn = ButtonList[i];
+                switch (btn.Text)
+                {
+                    // duong di
+                    case "1":
+                        Map.Add(1);
+                        break;
+                    // start
+                    case "-1":
+                        Map.Add(-1);
+                        break;
+                    // stop
+                    case "2":
+                        Map.Add(2);
+                        break;
+                    // reset
+                    case "3":
+                        Map.Add(3);
+                        break;
+                    case "":
+                        Map.Add(0);
+                        break;
+                }
+            }
+            for (int i = 0; i < 36; i++)
+            {
+                MapRow.Add(Map.GetRange(count, 64));
+                count += 64;
+                text += "{" + String.Join(",", MapRow[i]) + "},\n";
+            }
+            text = text.Substring(0, text.Length - 2) + "\n};";
+            Clipboard.SetText(text);
+            MessageBox.Show("xong roi =))))");
+        }
+    }
+}
